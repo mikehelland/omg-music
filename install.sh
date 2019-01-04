6f0c30642821df2271d75e189720dd39ed7dbc64
mikehelland <mike@mikehelland.com>
2019-01-03 21:08:21 -0800

    ask for pw in install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 
-## IMPORANT! Set the password for the database in this environment variable
-## It is used by the app and by the ./create_database.sh
+echo IMPORTANT! This requires Postgresql 9.4 or later!
+echo This will install Postgresql if there isn't a version installed
+echo But you should check to see if it's 9.4 or later
 
-export OMG_DB_PW=password_here
-
-## IMPORTANT! This requires Postgresql 9.4 or later!
-## This will install Postgresql if there isn't a version installed
-## But you should check to see if it's 9.4 or later
+echo "Give a db password"
+read -s password
+export OMG_DB_PW=$password
 
 which psql
 if [ "$?" -gt "0" ]; then
@@ -15,14 +14,12 @@ else
   echo "Postgresql is installed. Hopefully 9.4 or later!"
 fi
 
-# Now we can create our database with the password in OMG_DB_PW
+echo creating database...
 
 ./create_database.sh
 
-# This will install all the node modules for the project
+echo create node modules for the project
 
-npm init
+npm install
 
-# And now we're good to go!
-
-node main.js
+echo run with node main.js
