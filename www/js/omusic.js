b5b238170a489fa88b21ab4e9b24c93d6e26f85d
mikehelland <mike@mikehelland.com>
2021-07-23 22:24:44 -0700

    add notenames to musiccontext

--- www/js/omusic.js
+++ www/js/omusic.js
@@ -936,6 +936,17 @@ OMusicContext.prototype.scales = [{name: "Major", value: [0, 2, 4, 5, 7, 9, 11]}
         {name: "Chromatic", value: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
     ];
 
+OMusicContext.prototype.noteNames = ["C-", "C#-", "D-", "Eb-", "E-", "F-", "F#-", "G-", "G#-", "A-", "Bb-", "B-",
+    "C0", "C#0", "D0", "Eb0", "E0", "F0", "F#0", "G0", "G#0", "A0", "Bb0", "B0",
+    "C1", "C#1", "D1", "Eb1", "E1", "F1", "F#1", "G1", "G#1", "A1", "Bb1", "B1",
+    "C2", "C#2", "D2", "Eb2", "E2", "F2", "F#2", "G2", "G#2", "A2", "Bb2", "B2",
+    "C3", "C#3", "D3", "Eb3", "E3", "F3", "F#3", "G3", "G#3", "A3", "Bb3", "B3",
+    "C4", "C#4", "D4", "Eb4", "E4", "F4", "F#4", "G4", "G#4", "A4", "Bb4", "B4",
+    "C5", "C#5", "D5", "Eb5", "E5", "F5", "F#5", "G5", "G#5", "A5", "Bb5", "B5",
+    "C6", "C#6", "D6", "Eb6", "E6", "F6", "F#6", "G6", "G#6", "A6", "Bb6", "B6",
+    "C7", "C#7", "D7", "Eb7", "E7", "F7", "F#7", "G7", "G#7", "A7", "Bb7", "B7",
+    "C8"];
+
 OMusicContext.prototype.getKeyCaption = function (keyParams) {
     var scaleName = "Major";
     if (keyParams && keyParams.scale) {
